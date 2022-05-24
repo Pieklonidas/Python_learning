76a617ae0df943ab8e3b3c348a0c2b678c3897b4
Pieklonidas <tymoteuszkarwackip@gmail.com>
2022-05-24 21:48:03 +0200

    Episodes 4-5

--- Machine_learning_learning/Regression/main.py
+++ Machine_learning_learning/Regression/main.py
@@ -1,7 +1,14 @@
 import math
-
+import numpy
 import pandas as pd
-import quandl
+import quandl, datetime
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -16,7 +23,52 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+# print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+
+# print(df.head())
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_Lately = X[-forecast_out:]
+X = X[:-forecast_out:]
+
 df.dropna(inplace=True)
-print(df.head())
+y = np.array(df['label'])
+
+
+# X = X[:-forecast_out+1]
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=10)  # 98%
+# clf = svm.SVR(kernel='poly')  # 79% | poly - 68%
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+# print(accuracy)
+
+forecast_set = clf.predict(X_Lately)
+
+# print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+print(df.tail())
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
